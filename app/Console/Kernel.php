497c7b65990e1ad728d728feca252506b0b4f8c1
SuperStar0718 <georgejohnson11305@gmail.com>
2022-10-29 12:11:15 +0530

    Added Command For Scrape data #1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('moon:coins_data')->everyFiveMinutes();
+        $schedule->command('moon:coins_data')->everyFiveMinutes();
         $schedule->command('moon:fear-greed')->dailyAt('05:00');
         $schedule->command('moon:twitter')->daily();
         $schedule->command('moon:telegram')->daily();
