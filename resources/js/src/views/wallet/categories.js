848f995816f7c33cf84721699099c3fd2a06fa61
SuperStar0718 <georgejohnson11305@gmail.com>
2022-11-07 15:50:03 +0500

    wallet folder added and change in redirection

---
+++ resources/js/src/views/wallet/categories.js
@@ -0,0 +1,29 @@
+const data = [{
+    value: null,
+    text: 'All'
+},
+{
+    value: 'currency',
+    text: 'Currency'
+},{
+    value: 'Financial',
+    text: 'Financial'
+},{
+    value: 'infrastructure',
+    text: 'Infrastructure'
+},{
+    value: 'media-and-entertainment',
+    text: 'Media and Entertainment'
+},{
+    value: 'payments',
+    text: 'Payments'
+},{
+    value: 'services',
+    text: 'Services'
+},{
+    value: 'social-media',
+    text: 'Social Media'
+},
+                
+]
+export default data;
