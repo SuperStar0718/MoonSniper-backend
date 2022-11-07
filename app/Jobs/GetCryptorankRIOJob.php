2f52e856523017166131ff76af9cf5389543055f
SuperStar0718 <georgejohnson11305@gmail.com>
2022-11-07 08:11:21 +0200

    Revert "dashboard issue has been fixed"
    This reverts commit dfd3bed79ee9450af27fd1ed4efa0055686287da.

---
+++ app/Jobs/GetCryptorankRIOJob.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\CoinsData;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class GetCryptorankRIOJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $existing_coin_ids = CoinsData::select('coin_id','current_price')->get();
+
+        $tokens_id = [];
+        $id_price = [];
+
+        foreach($existing_coin_ids as $token) {
+            array_push($tokens_id,$token->coin_id);
+            $id_price[$token->coin_id] = $token->current_price;
+        }
+
+
+        $url = 'https://api.cryptorank.io/v0/coins/crowdsales';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15") );
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result= curl_exec ($ch);
+        curl_close ($ch);
+        $tokens_with_roi = json_decode($result, true);
+
+        $newCoinsArray = [];
+
+        foreach ($tokens_with_roi['data'] as $token_name => $token_data) {
+            if(in_array($token_name,$tokens_id)){
+
+                for($rounds=1;$rounds<=count($token_data);$rounds++){
+
+                    if (array_key_exists('price', $token_data[count($token_data)-$rounds])) {
+                        //lets use this price
+
+                        if(isset($token_data[count($token_data)-$rounds]['price']['USD'])) {
+                            $init_price = $token_data[count($token_data) - $rounds]['price']['USD'];
+                            $price_today = $id_price[$token_name];
+                            $ROI = round($price_today / $init_price, 2);
+                            $roi_percentage = $ROI * 100 - 100;
+                            $newCoinsArray[] = array(
+                                'coin_id' => $token_name,
+                                'roi_times' => $ROI,
+                                'roi_currency' => 'usd',
+                                'roi_percentage' => $roi_percentage
+                            );
+                        }//else we dont have other coins to compare with!
+                    }
+                }
+            }
+        }
+
+
+        //push to DB:
+        Try {
+            CoinsData::massUpdate(
+                values: $newCoinsArray,
+                uniqueBy: 'coin_id'
+            );
+        }catch (\Exception $exception){
+            Log::info("The Problem here is: ".$exception);
+        }
+        
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ app/Notifications/NotifyTokenUnlockNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NotifyTokenUnlockNotification extends Notification
+{
+    use Queueable;
+    public $token;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return  ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+        // public function toMail($notifiable)
+        // {
+        //     return (new MailMessage)
+        //                 ->line('The introduction to the notification.')
+        //                 ->action('Notification Action', url('/'))
+        //                 ->line('Thank you for using our application!');
+        // }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'name' => $this->token->name,
+            'coin_id' => $this->token->coin_id,
+            'next_unlock_date' => $this->token->next_unlock_date,
+            'next_unlock_number_of_tokens' => $this->token->next_unlock_number_of_tokens,
+            'symbol' => $this->token->symbol,
+            'image' => $this->token->image,
+            'current_price' => $this->token->current_price,
+        ];
+    }
+}
