53725d46dfb2c88c217b0f66d4981204c2ef8438
SuperStar0718 <georgejohnson11305@gmail.com>
2022-11-30 18:43:34 +0530

    extention coin  order by

--- app/Http/Controllers/ExchangesController.php
+++ app/Http/Controllers/ExchangesController.php
@@ -28,7 +28,7 @@ class ExchangesController extends Controller
     {
         $query = DB::table('coins');
 
-        $data = $query->leftJoin('coin_data', 'coins.symbol', '=', 'coin_data.symbol')->where('coins.symbol',$request->coin)->first();
+        $data = $query->leftJoin('coin_data', 'coins.symbol', '=', 'coin_data.symbol')->orderBy('coin_data.total_volume','DESC')->where('coins.symbol',$request->coin)->first();
         if($data)
         {
             return response()->json(['status'=>true,'coin'=>$data]);
