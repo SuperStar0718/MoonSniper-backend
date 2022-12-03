efb923ed3a87753d62474ae0dbaed36b4ee6d269
SuperStar0718 <georgejohnson11305@gmail.com>
2022-12-03 15:27:55 +0530

    Job Changes for Coin gecko

--- app/Jobs/GetIdForCoinDataJob.php
+++ app/Jobs/GetIdForCoinDataJob.php
@@ -33,7 +33,8 @@ class GetIdForCoinDataJob implements ShouldQueue
      * @return void
      */
     public function handle()
-    { $html = file_get_contents('https://www.coingecko.com/?page=1');
+    {
+        $html = file_get_contents('https://www.coingecko.com/?page=1');
         $pagination = $this->getBetween($html, '<ul class="pagination">', '</ul>');
         $docPage = new DOMDocument();
         $docPage->loadHTML($pagination);
@@ -41,12 +42,38 @@ class GetIdForCoinDataJob implements ShouldQueue
         $query = "//a";
         $entriesPage = $xpath->query($query);
         $totalPages =  $entriesPage[count($entriesPage)-2]->textContent;
-                $i=0;
-               for ($j=1; $j <= $totalPages ; $j++) { 
-                   $job = (new coinIdDataScrapingJob($j))->onQueue('moon-sniper-worker-long')->delay(now()->addseconds($i));
-                   $i=$i+5;
-                   dispatch($job);
-               }
+        for ($j=1; $j <= $totalPages ; $j++) { 
+            $html = file_get_contents('https://www.coingecko.com/?page='.$j.'');
+            $data = $this->getBetween($html, '<tbody', '</tbody>');
+            $doc = new DOMDocument();
+            $doc->loadHTML($data);
+            $xpath = new DOMXPath($doc);
+            $query = "//i";
+            $entries = $xpath->query($query);
+            $items = array();
+            foreach ($entries as $key => $entry) {
+                $items2 = array(
+                    'coingeckoid'=>$entry->getAttribute("data-coin-id"),
+                );
+                
+                try {
+                    $exist = CoinsData::where('symbol',$entry->getAttribute("data-coin-symbol"))->first();
+                    if($exist)
+                    {
+                        $coinData  = CoinsData::where('symbol',$entry->getAttribute("data-coin-symbol"))->update($items2);
+
+                    }else{
+                        $coinData  = CoinsData::where('coin_id',$entry->getAttribute("data-coin-slug"))->update($items2);
+
+                    }
+                } catch (\Throwable $th) {
+                    $coinData  = CoinsData::where('symbol',$entry->getAttribute("data-coin-symbol"))->orWhere('coin_id',$entry->getAttribute("data-coin-slug"))->update($items2);
+                }
+
+                // $items[] = $items2;
+            }
+           
+        }
       
     }
     public static function getBetween($content, $start, $end)
