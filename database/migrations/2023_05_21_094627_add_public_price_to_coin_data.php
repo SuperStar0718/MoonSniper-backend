2bf1cf420036bf202f2dbd800eea3a6508f34e5e
SuperStar0718 <georgejohnson11305@gmail.com>
2023-05-21 13:53:42 +0300

    Add migrations for public date and price

---
+++ database/migrations/2023_05_21_094627_add_public_price_to_coin_data.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coin_data', function (Blueprint $table) {
+            $table->double('public_price')->after('type');
+            $table->dateTime('public_date')->nullable()->after('public_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coin_data', function (Blueprint $table) {
+            $table->dropColumn('public_price');
+            $table->dropColumn('public_date');
+        });
+    }
+};
